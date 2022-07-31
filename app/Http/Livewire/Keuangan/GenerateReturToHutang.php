d7f30e83bde89266bc1b64524cc8881479172712
karyakirana <adibpratama2@gmail.com>
2022-07-31 19:24:11 +0700

    update PC

---
+++ app/Http/Livewire/Keuangan/GenerateReturToHutang.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Keuangan;
+
+use Livewire\Component;
+
+class GenerateReturToHutang extends Component
+{
+    public function render()
+    {
+        return view('livewire.keuangan.generate-retur-to-hutang');
+    }
+}
